030d0970da06f201212120544f9e3916a804e394
KM Rejoan Tanjim <kmrejoantanjim@gmail.com>
2025-09-26 23:15:31 +0600

    created user controller

---
+++ controllers/user.controller.js
@@ -0,0 +1,33 @@
+import { User } from "../models/user.js";
+import bcrypt from "bcrypt";
+export const register = async (req, res) => {
+  try {
+    const { fullName, email, password } = req.body;
+    if (!fullName || !email || !password) {
+      return res.status(403).json({
+        success: false,
+        message: "All field are required",
+      });
+    }
+    const user = await User.findOne(email);
+    if (user) {
+      return res.status(403).json({
+        success: false,
+        message: "user already registered",
+      });
+    }
+    const hashedPassword = bcrypt.hash(password, 10);
+    await User.create({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "accout created successfully",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
